b81921b144c09697209b5d3f8e1196a8c9422be0
Matija077 <matija.prs@gmail.com>
2020-03-21 16:37:28 +0100

    working node

--- node.js
+++ node.js
@@ -35,11 +35,31 @@ var NodeNode = (function() {
     var privateData = new WeakMap();
 
     function Node(value) {
-        privateData.set(this, { value: value});
+        privateData.set(this,
+            {
+                value: value,
+                next: null
+            });
     }
 
-    Node.prototype.getvalue = function() {
-        return privateData.get(this).name;
+    Node.prototype.getValue = function() {
+        return privateData.get(this).value;
+    }
+
+    Node.prototype.getNext = function() {
+        return privateData.get(this).next;
+    }
+
+    Node.prototype.setValue = function(value) {
+        privateData.get(this).value = value;
+    }
+
+    Node.prototype.setNext = function(next) {
+        if (!next instanceof Node) {
+            throw new Error(`next must be instance of ${Node} class`);
+        }
+
+        privateData.get(this).next = next;
     }
 
     return Node;
