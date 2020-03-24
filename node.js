63858e8c6b6a63b0064de98a4e1fc6b80730371f
Matija077 <matija.prs@gmail.com>
2020-03-24 12:43:16 +0100

    trying to add doublelInekd list
    but it is not wokring becasue i want pure functions, not closures

--- node.js
+++ node.js
@@ -38,7 +38,8 @@ var NodeNode = (function() {
         privateData.set(this,
             {
                 value: value,
-                next: null
+                next: null,
+                previous: null,
             });
         this.descriptor = value;
     }
@@ -52,6 +53,11 @@ var NodeNode = (function() {
         return privateData.get(this).next;
     }
 
+    Node.prototype.getPrevious = function() {
+        //console.log(privateData);
+        return privateData.get(this).previous;
+    }
+
     Node.prototype.setValue = function(value) {
         privateData.get(this).value = value;
     }
@@ -62,6 +68,14 @@ var NodeNode = (function() {
         }
 
         privateData.get(this).next = next;
+    }
+
+    Node.prototype.setPrevious = function(previous) {
+        if (!next instanceof Node) {
+            throw new Error(`previous must be instance of ${Node} class`);
+        }
+
+        privateData.get(this).previous = previous;
     }
 
     return Node;
