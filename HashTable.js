c6eb0300d4369760f4a4a259e7e29c9ce3872319
Matija077 <matija.prs@gmail.com>
2020-03-28 14:47:01 +0100

    addign hashtable

---
+++ HashTable.js
@@ -0,0 +1,12 @@
+var HashTablePublic = (function() {
+    var privateData = new WeakMap();
+
+    function HashTable() {
+        privateData.set(this, {
+
+        });
+    }
+
+
+    return HashTable;
+})()
